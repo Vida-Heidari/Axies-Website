2bd2495c47034d08d8261373decb506c8afc931f
Vida Heidari <vidaaheidari@gmail.com>
2022-03-22 01:30:17 +0430

    changes in styles added and log in page created

--- script.js
+++ script.js
@@ -34,10 +34,69 @@ function myDarkFunction() {
 }
 
 //---------------------------- Search Button ------------------------------
-document.querySelector(".search-icon").addEventListener("click", function () {
-  const searchButton = document.querySelector(".navbar__search-icon");
-  searchButton.classList.toggle("navbar__search-icon--open");
+let y = window.matchMedia("(min-width: 1201px)");
 
-  const searchActive = document.querySelector(".search-btn");
-  searchActive.classList.toggle("search-btn--active");
+document.querySelector(".search-icon").addEventListener("click", function () {
+  if (y.matches) {
+    const searchButton = document.querySelector(".navbar__search-icon");
+    searchButton.classList.toggle("navbar__search-icon--open");
+
+    const searchActive = document.querySelector(".search-icon");
+    searchActive.classList.toggle("search-btn--active");
+  }
+});
+
+// --------------------------------------------------------------------------
+// const navItems = document.querySelectorAll(".nav__link");
+// const navSubmenu = document.querySelectorAll(".nav__submenu");
+
+// for (let i = 0; i < navItems.length; i++) {
+//   navItems[i].addEventListener("click", function () {
+//     // for (let j = 0; j < navSubmenu.length; j++) {
+//     navSubmenu[i].classList.toggle("nav__submenu--open");
+//     // }
+//   });
+// }
+// //-----------------------------------
+// const navSubItems = document.querySelectorAll(".nav__subitem");
+// const navSubmenu2 = document.querySelectorAll(".nav__submenu2");
+
+// for (let i = 0; i < navSubItems.length; i++) {
+//   const navLink = navSubItems[i].querySelector(".nav__link");
+//   navLink.addEventListener("click", function () {
+//     // for (let j = 0; j < navSubmenu.length; j++) {
+//     navSubmenu2[i].classList.toggle("nav__submenu2--open");
+//     // }
+//   });
+// }
+//----------------------------------------------------------------------------------------
+//new
+const navLinks = document.querySelectorAll(".nav__link");
+let x = window.matchMedia("(max-width: 1200px)");
+
+navLinks.forEach(function (item) {
+  item.addEventListener("click", function () {
+    if (x.matches) {
+      if ([...item.parentNode.classList].includes("nav__item")) {
+        const navSubMenu = item.parentNode.querySelector(".nav__submenu");
+        navSubMenu.classList.toggle("nav__submenu--open");
+      } else if ([...item.parentNode.classList].includes("nav__subitem")) {
+        const navSubMenu2 = item.parentNode.querySelector(".nav__submenu2");
+        navSubMenu2 && navSubMenu2.classList.toggle("nav__submenu2--open");
+      }
+    }
+  });
+});
+
+//---------------------------------- Responsive menu button--------------------------------
+const navBtn = document.querySelector("#nav-btn");
+const navbar = document.querySelector(".header__navbar");
+const navbarClose = document.querySelector("#nav__close-btn");
+
+navBtn.addEventListener("click", function () {
+  navbar.classList.add("header__navbar--open");
+});
+
+navbarClose.addEventListener("click", function () {
+  navbar.classList.remove("header__navbar--open");
 });
