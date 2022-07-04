4d825101db66d7b4465533b93296e8462da44b5c
Vida Heidari <vidaaheidari@gmail.com>
2022-07-04 22:17:28 +0430

    JavaScript: added flat-tab scripts

---
+++ JavaScript/itemDetailes/tab.js
@@ -0,0 +1,25 @@
+"use stict";
+
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    //Change Active Tabe Color
+    tabs.forEach((e) => {
+      e.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    // Tab We Clicked on
+    const target = document.querySelector(tab.dataset.tabTarget);
+
+    // Making All The Tabs Disappear
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active-content");
+    });
+
+    // Only Show the Content Of That Tab we Clicked
+    target.classList.add("active-content");
+  });
+});
